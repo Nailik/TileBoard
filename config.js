e911cf2819428ec7af5d9491d71d9256ad53c4c3
Kilian Eller <kilianeller12@outlook.com>
2019-03-17 00:14:04 +0100

    fixed height

--- config.js
+++ config.js
@@ -77,6 +77,7 @@ var CONFIG = {
                      type: TYPES.IFRAME,
                      id: {},
                      width: 3,
+                     height: 3,
                      url: 'https://embed.windy.com/embed2.html?lat=49.870&lon=9.673&zoom=8&level=surface&overlay=temp&menu=&message=&marker=&calendar=&pressure=&type=map&location=coordinates&detail=true&detailLat=50.174&detailLon=9.673&metricWind=default&metricTemp=default&radarRange=-1'
                   }
                ]
